9bf75979229cafe209ea36c25c08bd0afad05ae5
Luis Pedro Coelho <lpc@cmu.edu>
2011-01-19 16:06:15 -0500

    ENH: Dirichlet models

--- elgreco/models.py
+++ elgreco/models.py
@@ -5,6 +5,23 @@
 
 from __future__ import division
 import numpy as np
+from scipy import stats
+
+class DirichletModel(object):
+    def __init__(self):
+        pass
+
+    def logP(self, value, alphas):
+        return np.dot(alphas, np.log(val))
+
+    def sample1(self, parents, children):
+        (parents,) = parents
+        children_stats = np.sum(children, axis=0)
+        children_stats += parents
+        gammas = np.array(map(stats.gamma.rvs, children_stats))
+        V = stats.gamma.rvs(children_stats.sum())
+        return gammas/V
+
 class ConstantModel(object):
     def __init__(self, value):
         self.value = value
--- elgreco/tests/test_models.py
+++ elgreco/tests/test_models.py
@@ -1,4 +1,5 @@
 from elgreco import models
+import numpy as np
 
 def test_constant_model():
     for v in (3, 2.22, -1):
@@ -7,3 +8,12 @@ def test_constant_model():
             assert c.sample1([], []) == v
         assert c.logP(v, []) == 0.
         assert c.logP(v+1, []) < -1000.
+
+def test_dirichlet_model():
+    np.random.seed(2)
+    d = models.DirichletModel()
+    alphas = np.zeros(3)+.1
+    children = [np.ones(3), np.ones(3)+3]
+    samples = np.sum([d.sample1([alphas], children) for i in xrange(1000)], axis=0)
+    assert samples.ptp() < 30
+
