6c2d607aca7f069b5a7cb0f4c7ddafd4d3211b4f
Luis Pedro Coelho <lpc@cmu.edu>
2011-01-28 13:48:11 -0500

    ENH C Code for LDA model
    This seems to work, although I have not yet figured out how to test it.
    
    One *huge* problem is that the generated files are gigantic: For LDA on
    the AP dataset, the output is 35M lines of code (859MB!). Not
    surprisingly, gcc refuses to compile this.

--- elgreco/compilation.py
+++ elgreco/compilation.py
@@ -9,7 +9,87 @@ Compilation
 ===========
 
 '''
+from collections import defaultdict
+import numpy as np
 from elgreco.graph import Node
+
+_module_header = '''
+extern "C" {
+    #include <Python.h>
+    #include <numpy/ndarrayobject.h>
+}
+#include <limits>
+
+namespace{
+
+const char ErrorMsg[] =
+    "El Greco internal error. Please report this as a bug.";
+
+
+struct random_source {
+    float uniform01() {
+        return random();
+    }
+    float sample_gamma(float n) {
+        return uniform01();
+    }
+};
+
+PyObject* py_gibbs(PyObject* self, PyObject* args) {
+    PyArrayObject* array;
+    if (!PyArg_ParseTuple(args,"O", &array)) return NULL;
+    if (!PyArray_Check(array) || PyArray_TYPE(array) != NPY_FLOAT || !PyArray_ISCARRAY(array)) {
+        PyErr_SetString(PyExc_RuntimeError, ErrorMsg);
+        return NULL;
+    }
+    float* data = static_cast<float*>(PyArray_DATA(array));
+    random_source R;
+
+'''
+
+_module_footer = '''
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyMethodDef methods[] = {
+  {"gibbs",(PyCFunction)py_gibbs, METH_VARARGS, NULL},
+  {NULL, NULL,0,NULL},
+};
+
+} // namespace
+extern "C"
+void init_elgreco()
+  {
+    import_array();
+    (void)Py_InitModule("_elgreco", methods);
+  }
+'''
+
+
+class Variables(object):
+    def __init__(self):
+        self.used = defaultdict(int)
+        self.decls = []
+
+    def allocate_float(self, basename, n=None):
+        return self.allocate(basename, 'float', n)
+
+    def allocate_int(self, basename, n=None):
+        return self.allocate(basename, 'int', n)
+
+    def allocate(self, basename, typename, n=None):
+        postfix = typename[0]
+        if n is not None:
+            postfix += 'a'
+        name = '%s_%s_%s' % (basename, self.used[basename], postfix)
+        self.used[basename] += 1
+        if n is None:
+            self.decls.append('%s %s;' % (typename, name))
+        else:
+            self.decls.append('%s %s[%s];' % (typename, name, int(n)))
+        return name
 
 class ArrayRef(object):
     def __init__(self, array, idx, n=None):
@@ -25,6 +105,11 @@ class ArrayRef(object):
     def set(self, value):
         self.array[self.slice] = value
 
+    def variable_name(self):
+        if type(self.slice) == int:
+            return '(data[%s])' % self.slice
+        return '(data+%s)' % self.slice.start
+
     def delete(self, obj):
         raise NotImplementedError('elgreco.compilation.ArrayRef.__delete__')
 
@@ -36,7 +121,7 @@ class NodeRef(Node):
     value = property(getval, setval)
 
 def arrayify(graph):
-    data = np.zeros(sum(v.model.size for v in graph.vertices))
+    data = np.zeros(sum(v.model.size for v in graph.vertices), dtype=np.float32)
     next = 0
     for v in graph.vertices:
         size = v.model.size
--- elgreco/models.py
+++ elgreco/models.py
@@ -2,12 +2,24 @@
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 #
 # LICENSE: GPLv3
+'''
+======
+Models
+======
+
+Each Model has a mirror ModelC which is the compiled version. The methods
+``compiled()`` and ``interpreted()`` go back and forth.
+'''
 
 from __future__ import division
 import numpy as np
 from scipy import stats
 
-class DirichletModel(object):
+
+def _variable_name(node):
+    return node._value.variable_name()
+
+class Dirichlet(object):
     dtype = np.ndarray
     def __init__(self, size):
         self.size = size
@@ -26,19 +38,95 @@ class DirichletModel(object):
             elif c.model.dtype == int:
                 alphas[c.value] += 1
             else:
-                raise ValueError('elgreco.models.DirichletModel: Cannot handle this type')
+                raise ValueError('elgreco.models.Dirichlet: Cannot handle this type')
         gammas = np.array(map(stats.gamma.rvs, alphas))
         V = stats.gamma.rvs(alphas.sum())
         return gammas/V
 
     def sampleforward(self, _n, parents):
         return self.sample1(_n, parents, [])
-
     def __str__(self):
-        return 'DirichletModel()'
+        return 'Dirichlet()'
     __repr__ = __str__
 
-class ConstantModel(object):
+    def compiled(self):
+        return DirichletC(self.size)
+
+
+
+class DirichletC(object):
+    dtype = np.ndarray
+    def __init__(self, size):
+        self.size = size
+    def logP(self, n, parents, result_var='result'):
+        (p,) = parents
+        alphas = _variable_name(p)
+        yield '''
+        {
+            float res = 0;
+            for (int i = 0; i != %(dim)s; ++i) {
+                res += %(alphas)s[i]*log(%(value)s);
+            }
+            %(result_var)s = res;
+        }
+''' %   {
+        'alphas' : alphas,
+        'value' : _variable_name(n),
+        'dim' : self.size,
+        'result_var' : result_var
+        }
+
+    def sample1(self, n, parents, children):
+        (p,) = parents
+        alphas = _variable_name(p)
+        result_var = _variable_name(n)
+        yield '''
+        {
+            float tmp_alphas[%(dim)s];
+            for (int i = 0; i != %(dim)s; ++i) {
+                tmp_alphas[i] = %(alphas)s[i];
+            }
+        ''' %   {
+                'dim' : self.size,
+                'alphas' : alphas,
+                }
+        for c in children:
+            if c.model.dtype == np.ndarray:
+                yield '''
+                for (int i = 0; i != %(dim)s; ++i) {
+                    tmp_alphas[i] += %(cvalue)s[i];
+                }
+                ''' % { 'dim' : self.size, 'cvalue' : _variable_name(c) }
+            elif c.model.dtype == int:
+                yield '''
+                ++tmp_alphas[int(%(pos)s)];
+                ''' % { 'pos' : _variable_name(c) }
+        yield '''
+            float sum_alphas;
+            for (int i = 0; i != %(dim)s; ++i) {
+                %(result_var)s[i] = R.sample_gamma(tmp_alphas[i]);
+                sum_alphas += tmp_alphas[i];
+            }
+            float V = R.sample_gamma(sum_alphas);
+            for (int i = 0; i != %(dim)s; ++i) {
+                %(result_var)s[i] /= V;
+            }
+        } ''' % {
+                'dim' : self.size,
+                'result_var' : result_var,
+                }
+
+    def sampleforward(self, n, parents):
+        return self.codesample1(self, n, parents, [])
+
+    def __str__(self):
+        return 'DirichletC()'
+    __repr__ = __str__
+
+    def interpreted(self):
+        return Dirichlet(self.size)
+
+class Constant(object):
     def __init__(self, value, base=None):
         self.value = value
         self.base = base
@@ -59,10 +147,78 @@ class ConstantModel(object):
     def sampleforward(self, _n, _p):
         return self.value
     def __str__(self):
-        return 'ConstantModel(%s)' % self.value
+        return 'Constant(%s)' % self.value
+
     __repr__ = __str__
 
-class FiniteUniverseModel(object):
+    def compiled(self):
+        return ConstantC(self.value, self.base)
+
+class ConstantC(Constant):
+
+    def logP(self, value, parents, result_var='result'):
+        if self.size == 1:
+            if self.base is not None:
+                compute_base = self.base.logP(value, parents, result_var)
+            else:
+                compute_base = '%s = 0.;' % result_var
+            yield '''
+                {
+                    float t = %(value)s;
+                    if (t != %(fixed)s) %(result_var)s = -std::numeric_limits<float>::infinity();
+                    else {
+                        %(compute_base)s
+                    }
+                } ''' % {
+                        'value' : value,
+                        'fixed' : self.value,
+                        'compute_base' : compute_base,
+                        'result_var' : result_var,
+                        }
+        else:
+            yield
+            '''
+            %(result_var)s = 0.;
+            ''' % { 'result_var' : result_var }
+            for i,v in enumerate(self.value):
+                yield '''
+                if (%(node)s[%(i)s] != %(value)s) {
+                    %(result_var)s = -std::numeric_limits<float>::infinity();
+                }''' % {
+                        'node' : _variable_name(n),
+                        'value' : self.value[i],
+                        'result_var' : result_var,
+                        'i' : i,
+                }
+            yield '''
+                if (%(result_var)s == 0.) {
+                    %(compute_base)s
+                }''' % {
+                        'result_var' : result_var,
+                        'compute_base' : self.base.logP(value, parents, result_var),
+                }
+
+    def sample1(self, _n, _p, _c):
+        yield '// constantC\n'
+
+    def sampleforward(self, n, parents):
+        if self.size == 1:
+            yield '%(node)s = %(value)s;' % {
+                            'node' : _variable_name(n),
+                            'value' : self.value,
+            }
+        else:
+            for i,v in enumerate(self.value):
+                yield '%(node)s[%(i)s] = %(value)s;\n' % {
+                        'node' : _variable_name(n),
+                        'value' : self.value[i],
+                        'i' : i,
+                }
+
+    def interpreted(self):
+        return Constant(self.value, self.base)
+
+class FiniteUniverse(object):
     def __init__(self, universe):
         self.universe = universe
 
@@ -90,11 +246,67 @@ class FiniteUniverseModel(object):
     def sampleforward(self, n, parents):
         return self.sample1(n, parents, [])
 
-class CategoricalModel(FiniteUniverseModel):
+class FiniteUniverseC(object):
+    def __init__(self, universe):
+        self.universe = universe
+
+    def sample1(self, n, parents, children):
+        yield '''
+        {
+            float ps[%(dim)s];
+            float t = 0.;
+        ''' % { 'dim' : len(self.universe) }
+        for i,v in enumerate(self.universe):
+            yield '''
+            ps[%(i)s] = 0;
+            %(node)s = %(v)s;
+            ''' % {
+                'i' : i,
+                'v' : v,
+                'node' : _variable_name(n),
+                }
+            for code in self.logP(_variable_name(n), parents, 'ps[%s]' % i):
+                yield code
+            for c in children:
+                for code in c.model.logP(_variable_name(c), c.children, 't'):
+                    yield code
+                yield 'ps[%s] += t;' % i
+        yield '''
+            float max_ps = -std::numeric_limits<float>::infinity();
+            for (int i = 0; i != %(dim)s; ++i) {
+                if (ps[i] > max_ps) max_ps = ps[i];
+            }
+            float sum_ps = 0.;
+            for (int i = 0; i != %(dim)s; ++i) {
+                ps[i] -= max_ps;
+                ps[i] = exp(ps[i]);
+                sum_ps += ps[i];
+            }
+            float r = R.uniform01();
+            int k = 0;
+            while (k < %(dim)s && r < ps[k + 1]) ++k;
+        ''' %   {
+                'dim' : len(self.universe),
+                }
+        for i,v in enumerate(self.universe):
+            yield '''
+            if (k == %(i)s) {
+                %(result_var)s = %(v)s;
+            } ''' % {
+                    'i' : i,
+                    'v' : v,
+                    'result_var' : _variable_name(n),
+            }
+        yield '''
+        }
+        '''
+
+
+class Categorical(FiniteUniverse):
     dtype = int
     size = 1
     def __init__(self, k):
-        FiniteUniverseModel.__init__(self, np.arange(k))
+        FiniteUniverse.__init__(self, np.arange(k))
 
     def logP(self, value, parents):
         (parents,) = parents
@@ -102,41 +314,115 @@ class CategoricalModel(FiniteUniverseModel):
         return np.log(alpha[value]/np.sum(alpha))
 
     def __str__(self):
-        return 'CategoricalModel(%s)' % len(self.universe)
+        return 'Categorical(%s)' % len(self.universe)
     __repr__ = __str__
 
-class BinomialModel(CategoricalModel):
+    def compiled(self):
+        return CategoricalC(len(self.universe))
+
+class CategoricalC(FiniteUniverseC):
+    dtype = int
+    size = 1
+    def __init__(self, k):
+        FiniteUniverseC.__init__(self, np.arange(k))
+
+    def logP(self, value, parents, result_var='result'):
+        (parent,) = parents
+        yield '''
+        {
+            float val = %(value)s;
+            float sum_alphas = 0;
+            for (int i = 0; i != %(dim)s; ++i) sum_alphas += %(alphas)s[i];
+            %(result_var)s = log(%(alphas)s[int(val)]/sum_alphas);
+        } ''' % {
+                'value' : value,
+                'alphas' : _variable_name(parent),
+                'result_var' : result_var,
+                'dim' : len(self.universe),
+        }
+
+    def interpreted(self):
+        return Categorical(len(self.universe))
+
+class Binomial(Categorical):
     dtype = bool
     def __init__(self):
-        CategoricalModel.__init__(self, 2)
+        Categorical.__init__(self, 2)
     def __str__(self):
-        return 'BinomialModel()'
+        return 'Binomial()'
     __repr__ = __str__
 
-class MultinomialModel(object):
+class Multinomial(object):
     dtype = np.ndarray
     def __init__(self, n):
         self.size = n
 
-class ChoiceModel(object):
+class Choice(object):
     def __init__(self, base):
         self.base = base
         self.dtype = self.base.dtype
         self.size = self.base.size
 
     def logP(self, value, parents):
-        return self.base.logP(value, self._select_parent)
+        return self.base.logP(value, self._select_parent(parents))
 
     def _select_parent(self, parents):
         switch = parents[0].value
         parent = parents[1+switch]
 
     def sample1(self, n, parents, children):
-        return self.base.sample1(n, self._select_parent(parents), children)
+        return self.base.sample1(n, [self._select_parent(parents)], children)
 
     def sampleforward(self, n, parents):
-        return self.base.sampleforward(n, self._select_parent(parent))
+        return self.base.sampleforward(n, [self._select_parent(parent)])
 
     def __str__(self):
-        return 'ChoiceModel(%s)' % self.base
+        return 'Choice(%s)' % self.base
 
+    def compiled(self):
+        return ChoiceC(self.base)
+
+def ChoiceC(object):
+    def __init__(self, base):
+        Choice.__init__(self, base)
+
+    def logP(self, value, parents, result_var='result'):
+        p0 = parents[0]
+        for i,p1 in enumerate(parents[:1]):
+            yield '''
+                if (%(p1)s == %(p0)s) {
+                    %(compute_base)s;
+                } ''' % {
+                'p0' : _variable_name(p0),
+                'p1' : _variable_name(p1),
+                'compute_base' : self.base.logP(value, p1, result_var),
+            }
+
+    def sample1(self, n, parents, children):
+        p0 = parents[0]
+        for i,p1 in enumerate(parents[:1]):
+            yield '''
+            if (%(p1)s == %(p0)s) {
+                %(compute_base)s;
+            } ''' % {
+                'p0' : _variable_name(p0),
+                'p1' : _variable_name(p1),
+                'compute_base' : self.base.sample1(value, p1, children)
+            }
+    def sampleforward(self, n, parents):
+        p0 = parents[0]
+        for i,p1 in enumerate(parents[:1]):
+            yield '''
+            if (%(p1)s == %(p0)s) {
+                %(compute_base)s;
+            } ''' % {
+                'p0' : _variable_name(p0),
+                'p1' : _variable_name(p1),
+                'compute_base' : self.base.sampleforward(value, p1)
+            }
+
+    def __str__(self):
+        return 'ChoiceC(%s)' % self.base
+
+    def interpreted(self):
+        return Choice(self.base)
