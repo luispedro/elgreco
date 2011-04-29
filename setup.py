11e06f633489b7cc44c267a3178d0f71280b4e2c
Luis Pedro Coelho <lpc@cmu.edu>
2011-04-29 19:19:15 -0400

    ENH Build Python extension

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011, Luis Pedro Coelho <luis@luispedro.org>
+
+from distutils.core import setup, Extension
+
+lda_module = Extension(
+                'elgreco._lda',
+                sources=['elgreco/lda_wrap.cxx', 'elgreco/lda.cpp', 'elgreco/load.cpp'],
+                libraries=['gsl', 'gslcblas'],
+                extra_compile_args=['-fopenmp'],
+                extra_link_args=['-lgomp'],
+               )
+
+setup (name = 'elgreco',
+       version = '0.1',
+       author = 'Luis Pedro Coelho <luis@luispedro.org>',
+       ext_modules = [lda_module],
+       py_modules = ['elgreco.lda'],
+       )
