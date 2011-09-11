4de4e59e56a46127eec0d796d395be86191c6358
Luis Pedro Coelho <lpc@cmu.edu>
2011-09-11 14:12:23 -0400

    DEMO Simplify. Improve code

--- elgreco/demos/sample_slda.py
+++ elgreco/demos/sample_slda.py
@@ -1,5 +1,10 @@
 import numpy as np
+from time import time as now
 from elgreco import lda
+
+nr_topics = 40
+nr_labels = 8
+nr_f = 0
 
 def load_data(datafile):
     documents = []
@@ -17,38 +22,45 @@ def load_data(datafile):
 labels = [int(line.strip()) for line in file('../train-label.dat')]
 
 data = lda.lda_data()
-labs = np.zeros(8, bool)
+labs = np.zeros(nr_labels, bool)
 for doc,lab in zip(load_data('../train-data.dat'), labels):
     labs[:] = 0
     labs[lab] = 1
-    fs = [np.random.random() for f in xrange(4)]
+    fs = [np.random.random() for f in xrange(nr_f)]
     data.push_back_doc(doc, fs, labs)
     
 params = lda.lda_parameters()
 params.alpha = .01
 params.beta = .1
-params.nr_topics = 40
-params.nr_labels = 8
+params.nr_topics = nr_topics
+params.nr_labels = nr_labels
 params.seed = 2
 sampler = lda.lda_collapsed(data, params)
 sampler.forward()
 
 logps = [sampler.logP()]
+#start = now()
 for i in xrange(100):
     sampler.step()
     lp = (sampler.logP())
     print lp
-    logps.append(sampler.logP())
+    logps.append(lp)
+#end = now()
+#print end-start
 
-tdocuments = load_data('test-data.dat')
-tlabels = [int(line.strip()) for line in file('test-label.dat')]
-G = np.zeros(40, np.float32)
+tdocuments = load_data('../test-data.dat')
+tlabels = [int(line.strip()) for line in file('../test-label.dat')]
+G = np.zeros(nr_topics, np.float32)
 sampler.retrieve_gamma(0,G)
-T = np.zeros(40, np.float32)
+T = np.zeros((len(tdocuments),nr_topics), np.float32)
 tests = []
-for doc in tdocuments:
-    fs = [np.random.random() for f in xrange(4)]
-    _ = sampler.project_one(doc, fs, T)
-    tests.append([sampler.score_one(ell,T) for ell in xrange(8)])
+for ti,doc in enumerate(tdocuments):
+    fs = [np.random.random() for f in xrange(nr_f)]
+    s = sampler.project_one(doc, fs, T[ti])
+    if s <= 0:
+        raise RuntimeError('something bad happened')
 
+    tests.append([sampler.score_one(ell,T[ti]) for ell in xrange(nr_labels)])
+tests = np.array(tests)
+print np.mean(tlabels == tests.argmax(1))
 
