86fab87a4863627621e4c9578742fce41ea30148
Luis Pedro Coelho <lpc@cmu.edu>
2011-09-25 15:17:21 -0400

    BUG Fix solve_gamma
    H need not be positive semi-definite

--- elgreco/lda.cpp
+++ elgreco/lda.cpp
@@ -744,6 +744,7 @@ void lda::lda_collapsed::solve_gammas() {
     scoped_array<double> gdata(new double[K_]);
     scoped_array<double> ddata(new double[K_]);
     scoped_array<double> Hdata(new double[K_*K_]);
+    gsl_permutation* permutation = gsl_permutation_alloc(K_);
 
     for (int i = 0; i != N_; ++i) {
         floating* zb = zbars.get() + K_*i;
@@ -757,28 +758,33 @@ void lda::lda_collapsed::solve_gammas() {
         std::fill(gdata.get(), gdata.get() + K_, 0.);
         std::fill(Hdata.get(), Hdata.get() + K_*K_, 0.);
         for (int i = 0; i != N_; ++i) {
-            const floating* zi = zbars.get() + K_*i;
-            const floating f_g = ls(i)[ell]*(8./9)*dot_product(gl, zi, K_);
-            const floating sq_tpi = 0.3989422804014327; // sqrt(1/2./pi)
-            const floating phi1 = sq_tpi*std::exp(-f_g*f_g/2.);
-            const floating phi2 = (-2.)*f_g*phi1;
-            for (int k = 0; k != K_; ++k) {
-                gdata[k] += zi[k]*phi1;
-                for (int m = 0; m != K_; ++m) {
-                    Hdata[k*K_+m] += zi[k]*zi[m]*phi2;
+            const floating* li = ls(i);
+            if (li[ell]) {
+                const floating* zi = zbars.get() + K_*i;
+                const floating f_g = li[ell]*(8./9)*dot_product(gl, zi, K_);
+                const floating sq_tpi = 0.3989422804014327; // sqrt(1/2./pi)
+                const floating phi1 = sq_tpi*std::exp(-f_g*f_g/2.);
+                const floating phi2 = (-2.)*f_g*phi1;
+                for (int k = 0; k != K_; ++k) {
+                    gdata[k] += zi[k]*phi1;
+                    for (int m = 0; m != K_; ++m) {
+                        Hdata[k*K_+m] += zi[k]*zi[m]*phi2;
+                    }
                 }
             }
         }
+        int signum;
         gsl_vector_view gvector = gsl_vector_view_array(gdata.get(), K_);
         gsl_vector_view dvector = gsl_vector_view_array(ddata.get(), K_);
         gsl_matrix_view Hmatrix = gsl_matrix_view_array(Hdata.get(), K_, K_);
-        gsl_linalg_cholesky_decomp(&Hmatrix.matrix);
-        gsl_linalg_cholesky_solve(&Hmatrix.matrix, &gvector.vector, &dvector.vector);
+        gsl_linalg_LU_decomp(&Hmatrix.matrix, permutation, &signum);
+        gsl_linalg_LU_solve(&Hmatrix.matrix, permutation, &gvector.vector, &dvector.vector);
 
         for (int k = 0; k != K_; ++k) {
             gl[k] += ddata[k];
         }
     }
+    gsl_permutation_free(permutation);
 }
 
 
