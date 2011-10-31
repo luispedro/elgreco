7eed94359665201b8ede2d368fa34c6267676627
Luis Pedro Coelho <luis@luispedro.org>
2011-10-31 17:38:56 +0200

    BLD numpy.distutils does not respect swig_opts
    With this new setup.py, elgreco can be built using the standard::
    
        python setup.py install

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2011, Luis Pedro Coelho <luis@luispedro.org>
 
-from numpy.distutils.core import setup, Extension
+from distutils.core import setup, Extension
 import os
 
 undef_macros=[]
@@ -10,16 +10,23 @@ if os.environ.get('DEBUG'):
 
 lda_module = Extension(
                 'elgreco._lda',
-                sources=['elgreco/lda_wrap.cxx', 'elgreco/lda.cpp', 'elgreco/load.cpp'],
+                sources=['elgreco/lda.i', 'elgreco/lda.cpp', 'elgreco/load.cpp'],
                 libraries=['gsl', 'gslcblas'],
                 extra_compile_args=['-fopenmp'],
                 extra_link_args=['-lgomp'],
                 undef_macros=undef_macros,
+                swig_opts=['-c++'],
                )
+
+random_module = Extension(
+                'elgreco.elgreco_random',
+                sources=['elgreco/elgreco_random.i'],
+                swig_opts=['-c++'],
+                )
 
 setup (name = 'elgreco',
        version = '0.1',
        author = 'Luis Pedro Coelho <luis@luispedro.org>',
-       ext_modules = [lda_module],
+       ext_modules = [lda_module, random_module],
        py_modules = ['elgreco.lda'],
        )
