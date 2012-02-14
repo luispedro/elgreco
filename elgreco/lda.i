eff2dccf1ce93ae2b7c1dc34f05a6f83125dba98
Luis Pedro Coelho <luis@luispedro.org>
2012-02-14 16:16:14 +0000

    ENH save/load to/from strings

--- elgreco/lda.i
+++ elgreco/lda.i
@@ -9,6 +9,7 @@
 
 %module lda
 %include "std_vector.i"
+%include "std_string.i"
 namespace std {
     %template(vectori) std::vector<int>;
     %template(vectord) std::vector<double>;
@@ -27,10 +28,22 @@ namespace std {
 %apply (float* IN_ARRAY1, int DIM1) {(const float* array, int size)}
 
 %{
+#include <sstream>
 #include "lda.h"
+
+std::string save_model_to_string(const lda::lda_base& lda) {
+    std::stringstream out;
+    lda.save_model(out);
+    return out.str();
+}
+
+void load_model_from_string(lda::lda_base& lda, const std::string& input) {
+    std::stringstream in(input);
+    lda.load_model(in);
+}
 %}
-
-
 %include "lda.h"
 
+std::string save_model_to_string(const lda::lda_base& lda);
+void load_model_from_string(lda::lda_base& lda, const std::string& input);
 
--- elgreco/tests/test_lda.py
+++ elgreco/tests/test_lda.py
@@ -22,3 +22,32 @@ def test_lda_empty_doc():
     assert logp < 0
 
 
+def test_save_load():
+    data = lda.lda_data()
+    data.push_back_doc(range(5), [], [])
+    data.push_back_doc(range(5,10), [], [])
+    data.push_back_doc(range(5,10), [], [])
+    data.push_back_doc(range(10), [], [])
+    data.push_back_doc(range(10), [], [])
+
+    params = lda.lda_parameters()
+    params.alpha = .01
+    params.beta = .1
+    params.nr_topics = 2
+    params.nr_labels = 0
+    params.seed = 2
+
+    sampler = lda.lda_collapsed(data, params)
+    sampler.forward()
+    for i in xrange(10): sampler.step()
+    logp = sampler.logP()
+    s = lda.save_model_to_string(sampler)
+    for i in xrange(10): sampler.step()
+    logp2 = sampler.logP()
+    lda.load_model_from_string(sampler, s)
+    logp3 = sampler.logP()
+    assert logp != logp2
+    assert logp == logp3
+    print logp, logp2, logp3
+
+
